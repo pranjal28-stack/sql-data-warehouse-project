fc761ac89b49e335c368094a721a0d077e1ae1ed
pranjal28-stack <singhpranjalvardhan@gmail.com>
2026-04-23 02:45:51 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -22,3 +22,68 @@ create database DataWarehouse;
 
 -- Use the database
 use DataWarehouse;
+
+-- LOAD CUSTOMER DATA (CRM)
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_crm/cust_info.csv'
+INTO TABLE bronze_crm_cust_info
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- =========================================
+-- LOAD PRODUCT DATA (CRM)
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_crm/prd_info.csv'
+INTO TABLE bronze_crm_prd_info
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- =========================================
+-- LOAD SALES DATA (CRM)
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_crm/sales_details.csv'
+INTO TABLE bronze_crm_sales_details
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- =========================================
+-- LOAD ERP LOCATION DATA
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_erp/loc_a101.csv'
+INTO TABLE bronze_erp_loc_a101
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+
+-- =========================================
+-- LOAD ERP CUSTOMER DATA (AZ12)
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_erp/cust_az12.csv'
+INTO TABLE bronze_erp_cust_az12
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- =========================================
+-- LOAD ERP CATEGORY DATA
+-- =========================================
+LOAD DATA LOCAL INFILE 'datasets/source_erp/px_cat_g1v.csv'
+INTO TABLE bronze_erp_px_cat_g1v
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
